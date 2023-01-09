311ef82ff9751e16a75a8391c949c459b3fcceac
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-09 14:05:40 -0300

    style

--- src/components/ListFinance/index.jsx
+++ src/components/ListFinance/index.jsx
@@ -7,38 +7,44 @@ export const ListFinance = () => {
   const { listItem, deleteTransaction, setIsModal } = useContext(FinaceContext);
 
   return (
-    <StyledList>
-      <h3>Transações</h3>
-      <div>
-        <h4>Descrição</h4>
-        <h4>Valor</h4>
-        <h4>Tipo</h4>
-      </div>
-      {listItem.length === 0 ? (
-        <p>Adicione novas transações</p>
-      ) : (
-        <ul>
-          {listItem?.map((item, index) => {
-            const { description, value, type } = item;
-            return (
-              <li>
-                <div>
-                  <p>{description}</p>
-                  <p>{value.toFixed()}</p>
-                  <p>{type}</p>
-                </div>
-                <button type="button" onClick={() => deleteTransaction(index)}>
-                  <img src={iconDelete} alt="" />
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-
+    <div>
+      <StyledList>
+        <h3>Transações</h3>
+        {listItem.length === 0 ? (
+          <p>Adicione novas transações</p>
+        ) : (
+          <section>
+            <div>
+              <h4>Descrição</h4>
+              <h4>Valor</h4>
+              <h4>Tipo</h4>
+            </div>
+            <ul>
+              {listItem?.map((item, index) => {
+                const { description, value, type } = item;
+                return (
+                  <li>
+                    <div>
+                      <span>{description}</span>
+                      <span>{value.toFixed()}</span>
+                      <span>{type}</span>
+                    </div>
+                    <button
+                      type="button"
+                      onClick={() => deleteTransaction(index)}
+                    >
+                      <img src={iconDelete} alt="" />
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        )}
+      </StyledList>
       <StyledButtonAdd>
         <button onClick={() => setIsModal(true)}>+</button>
       </StyledButtonAdd>
-    </StyledList>
+    </div>
   );
 };
--- src/components/ListFinance/style.js
+++ src/components/ListFinance/style.js
@@ -4,25 +4,33 @@ export const StyledList = styled.section`
   background-color: var(--color-grey-0);
   border-radius: 4px;
   max-width: 800px;
+  border: 1px solid grey;
 
   h3 {
     text-align: center;
     margin-bottom: 20px;
+    border-bottom: 1px solid grey;
   }
 
   div {
     width: 70%;
     display: flex;
     justify-content: space-between;
-
+    
     h4 {
       width: 80px;
+      padding: 0 5px;
     }
+  }
+
+  p {
+    text-align: center;
+    padding: 20px;
   }
 
   ul {
     width: 100%;
-    max-height: 160px;
+    height: 200px;
     margin-top: 15px;
     display: flex;
     flex-direction: column;
@@ -32,16 +40,16 @@ export const StyledList = styled.section`
 
     li {
       width: 100%;
-      padding: 10px 0;
+      padding: 10px 5px;
       display: flex;
       justify-content: space-between;
-      border-bottom: 1px solid grey;
+      background-color: #80808024;
 
       div {
         width: 70%;
         display: flex;
 
-        p {
+        span {
           width: 80px;
           overflow: hidden;
           text-overflow: ellipsis;
@@ -62,20 +70,19 @@ export const StyledList = styled.section`
       border: none;
     }
   }
-
 `;
 
 export const StyledButtonAdd = styled.section`
-display: flex;
-align-items: flex-end;
+  display: flex;
+  align-items: flex-end;
 
-button {
-  width: 50px;
-  height: 50px;
-  font-size: 30px;
-  border: none;
-  border-radius: 50px;
-  color: var(--color-grey-0);
-  background-color: var(--color-primary);
-}
+  button {
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+    border: none;
+    border-radius: 50px;
+    color: var(--color-grey-0);
+    background-color: var(--color-primary);
+  }
 `;
